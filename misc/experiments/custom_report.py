61047475d89d4e99fd57c77185e69d35c51a4d48
Patrick Weber <pa.weber01@gmail.com>
2026-01-19 10:57:45 +0000

    Update custom_report.py

--- misc/experiments/custom_report.py
+++ misc/experiments/custom_report.py
@@ -14,6 +14,7 @@ def create_comparative_report(*args):
     for config_nick, _ in config_nicks:
         configs.append(config_nick)
 
+    count = 0
     for i in range(len(configs) // n):
         algorithms = []
         for rev in revisions:
@@ -22,14 +23,25 @@ def create_comparative_report(*args):
 
         revision_pairs = [(rev1, rev2) for rev1, rev2 in itertools.combinations(algorithms, 2)]
 
-        for j, (rev1, rev2) in enumerate(revision_pairs):
-                algorithm_pairs.append(
-                    (
-                        rev1,
-                        rev2,
-                        f"Diff {i * n + j}"
-                    )
+        for rev1, rev2 in revision_pairs:
+            if not ("unrolling" in rev1 or "unrolling" in rev2):
+                continue
+            if (not ("-add-" in rev1 and "-add-" in rev2)) and (not ("-ff-" in rev1 and "-ff-" in rev2)) and (not ("-lm-" in rev1 and "-lm-" in rev2)):
+                continue
+            # Ensure unrolling is always on the left side
+            if "unrolling" in rev2: 
+                rev2_temp = rev1
+                rev1 = rev2
+                rev2 = rev2_temp
+
+            algorithm_pairs.append(
+                (
+                    rev1,
+                    rev2,
+                    f"Diff {count}"
                 )
+            )
+            count += 1
     report = ComparativeReport(algorithm_pairs, attributes=attributes)
     outfile = os.path.join(
     exp.eval_dir,
@@ -72,33 +84,47 @@ def create_scatter_plot_report(*args):
                     ylabel = "Approximate Negative Cycles"
                 elif "approximate" in rev2:
                     ylabel = "Approximate Negative"
+                print(f"xlabel: {xlabel}, ylabel: {ylabel}")
                 if not "Unrolling" in [xlabel, ylabel]:
                     continue
                 
-                if "-add-" in rev1:
+                if "-add-" in rev1 and "-add-" in rev2:
                     algo = "Add"
-                elif "-ff-" in rev1:
+                elif "-ff-" in rev1 and "-ff-" in rev2:
                     algo = "FF"
-                elif "-lm-" in rev1:
+                elif "-lm-" in rev1 and "-lm-" in rev2:
                     algo = "LM"
-                    
+                else:
+                    continue
+                
+                # Ensure unrolling is always on the y-axis
+                if xlabel == "Unrolling": 
+                    xlabel_temp = ylabel
+                    ylabel = xlabel
+                    xlabel = xlabel_temp
+                    rev1_temp = rev2
+                    rev2 = rev1
+                    rev1 = rev1_temp
+
                 algorithm_pairs.append(
                     (
                         rev1,
                         rev2,
-                        f"Diff {i * n + j}"
+                        xlabel,
+                        ylabel,
+                        algo
                     )
                 )
     for attribute in attributes:
         for algorithm_pair in algorithm_pairs:
             report = ScatterPlotReport(attributes=attribute, 
                                        filter_algorithm=[algorithm_pair[0], algorithm_pair[1]], 
-                                       xlabel=xlabel, 
-                                       ylabel=ylabel, 
+                                       xlabel=algorithm_pair[2], 
+                                       ylabel=algorithm_pair[3], 
                                        get_category=domain_as_category)
             outfile = os.path.join(
             exp.eval_dir,
             "%s-%s-%s-%s.%s" % (
-                xlabel, ylabel, algo, attribute, report.output_format))
+                algorithm_pair[2], algorithm_pair[3], algorithm_pair[4], attribute, report.output_format))
             report(exp.eval_dir, outfile)
 
